707bb270847b998bc49af95f52352af5d7549d9e
Fendy Purnomo <fendypurnomo@outlook.com>
2025-03-01 21:17:44 +0700

    Menambah variable port

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,8 @@ async function bootstrap() {
       stopAtFirstError: true
     })
   );
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 
 bootstrap();
