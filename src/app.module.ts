d9973f3efe764427c9df57cf0e23a1ae37e10f96
Fendy Purnomo <fendypurnomo@outlook.com>
2025-03-03 17:11:06 +0700

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
-import {Module} from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config';
-import {MongooseModule} from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import {AuthModule} from './apps/auth/auth.module';
-import {BlogModule} from './apps/blog/blog.module';
-import {HomeModule} from './apps/home/home.module';
-import {UserModule} from './apps/user/user.module';
+import { AuthModule } from './apps/auth/auth.module';
+import { BlogModule } from './apps/blog/blog.module';
+import { HomeModule } from './apps/home/home.module';
+import { UserModule } from './apps/user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(`${process.env.DB_URI_ATLAS}`),
-    AuthModule,
-    BlogModule,
-    HomeModule,
-    UserModule
-  ]
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(`${process.env.MONGO_URI_RAILWAY}`),
+        AuthModule,
+        BlogModule,
+        HomeModule,
+        UserModule
+    ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
-import {ValidationPipe} from '@nestjs/common';
-import {NestFactory} from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
-import {AppModule} from './app.module';
-import {customExceptionFactory} from './helpers/exception.factory';
+import { AppModule } from './app.module';
+import { customExceptionFactory } from './helpers/exception.factory';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+    const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      exceptionFactory: customExceptionFactory,
-      stopAtFirstError: true
-    })
-  );
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            exceptionFactory: customExceptionFactory,
+            stopAtFirstError: true
+        })
+    );
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
 }
 
 bootstrap();
