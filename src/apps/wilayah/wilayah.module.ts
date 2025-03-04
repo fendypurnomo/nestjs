1d9a68585d0c06584bbd9245eeac0902e3a762aa
Fendy Purnomo <fendypurnomo@outlook.com>
2025-03-04 23:31:22 +0700

    Tambahkan module `wilayah`

---
+++ src/apps/wilayah/wilayah.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { ProvinsiController } from './provinsi.controller';
+import { ProvinsiService } from './provinsi.service';
+
+@Module({
+    controllers: [ProvinsiController],
+    providers: [ProvinsiService]
+})
+export class WilayahModule {}
