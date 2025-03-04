99bb24bcc16085d8087c9259a1be87312d0b39cb
Fendy Purnomo <fendypurnomo@outlook.com>
2025-03-04 23:33:43 +0700

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,18 @@ import { AuthModule } from './apps/auth/auth.module';
 import { BlogModule } from './apps/blog/blog.module';
 import { HomeModule } from './apps/home/home.module';
 import { UserModule } from './apps/user/user.module';
+import { WilayahModule } from './apps/wilayah/wilayah.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
+        // Koneksi ke MongoDB
         MongooseModule.forRoot(`${process.env.MONGO_URI_RAILWAY}`),
         AuthModule,
         BlogModule,
         HomeModule,
-        UserModule
+        UserModule,
+        WilayahModule
     ]
 })
 export class AppModule {}
